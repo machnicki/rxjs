d9791478092f70a86baa6e4333c3b8fe9ec19cb3
Miron Machnicki <miron@machnicki.com.pl>
2017-01-13 21:16:48 +0000

    Basic operations

--- src/samples/samples.jsx
+++ src/samples/samples.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Http from 'my-http'
+import Rx from 'rxjs'
 
 export default function Samples() {
-  Http({ name: 'cars' }).get().then(resp => console.log(resp))
+  const cars$ = Rx.Observable.fromPromise(Http({ name: 'cars' }).get())
+
+  cars$.subscribe(
+    data => Rx.Observable.from(data)
+    .subscribe(item => console.log('item', item))
+  )
 
   return (<div>
     This is my sample
