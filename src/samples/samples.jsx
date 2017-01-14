af21a90c3adc6f9af931fd7358977ebaaff94f6f
Miron Machnicki <miron@machnicki.com.pl>
2017-01-14 19:36:28 +0000

    Merge map plus some new methods

--- src/samples/samples.jsx
+++ src/samples/samples.jsx
@@ -3,12 +3,25 @@ import Http from 'my-http'
 import Rx from 'rxjs'
 
 export default function Samples() {
-  const cars$ = Rx.Observable.fromPromise(Http({ name: 'cars' }).get())
+  const cars$ = Rx.Observable
+    .fromPromise(Http({ name: 'cars' }).get())
+    .mergeMap(data => Rx.Observable.from(data))
 
-  cars$.subscribe(
-    data => Rx.Observable.from(data)
-    .subscribe(item => console.log('item', item))
-  )
+    // or just create and manually trigger next() and complete()
+    // subscribeOn(Rx.Scheduler.async or queue)
+    // unsubscribe if done
+
+    // actions && events (by Rx.Subject) for flux flow
+    // start with && scan
+    // sometimes use combineLatest instead of merge
+
+  cars$
+    .pluck('name')
+    .subscribe(
+      item => console.log('item', item),
+      er => console.error(err),
+      () => console.log('done!')
+    )
 
   return (<div>
     This is my sample
